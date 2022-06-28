2852d40cefd191f2dbed40cdae2cc0c3d03b174a
domstrumidlo <108329062+domstrumidlo@users.noreply.github.com>
2022-06-28 10:54:32 +0200

    Add files via upload
    #script makes recursive searches for content of specific files in given directory and change strings

---
+++ mass_String_replace_recursive.ps1
@@ -0,0 +1,38 @@
+﻿#author: Dominik Strumidlo
+#script recurivly searches for content of specific files in given directory and change strings
+$SearchPath = ""
+$filesArray = New-Object System.Collections.Generic.List[System.Object]
+
+
+#get files in directory
+function directorySearch($directoryPath = $pwd, [string[]]$exclude)
+{
+    foreach ($item in Get-ChildItem $directoryPath)
+    {
+        if ($exclude | Where {$item -like $_}) { continue }
+        if (Test-directoryPath $item.FullName -directoryPathType Container)
+        {
+            $filesArray.Add($item.FullName)
+            directorySearch $item.FullName $exclude
+        }
+    }
+} 
+
+#function call
+directorySearch($SearchPath)
+
+$oldString = "aa"
+$newString = "bb"
+#in each folder search for file with name, and change extension
+foreach ($items in $filesArray)
+{
+    $fulldirectoryPath = $items + $oldString #ex. files with extension
+    $filenames = @(get-childitem -directoryPath $fulldirectoryPath | % { $_.FullName })
+    
+        foreach ($file in $filenames) 
+        {
+            $replacementStr = $newString #ex. new extension
+           (Get-Content $file) | Foreach-object { $_ -replace $oldString , $replacementStr   } | Set-Content $file
+             Write-Host Processed $file #write changes
+       }
+}
