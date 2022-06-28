523b82f10f06399dcf67b0b72bb71997a7f83afe
domstrumidlo <108329062+domstrumidlo@users.noreply.github.com>
2022-06-28 10:24:46 +0200

    Add files via upload
    This script search only CLOUD users (Azure AD, Exchange) and forces those users to change their password at logon
    Usable when u have hybrid AD (on-prem, azure ad) env

---
+++ ForceCloudUsersO365PasswordChange.ps1
@@ -0,0 +1,35 @@
+﻿#author: Dominik Strumidlo
+#dominik@strumidlo.pl
+#this script forces cloud (Exchange O365) users to change their password at logon
+
+#Initiates a connection to Azure Active Directory.
+Connect-MsolService 
+Import-Module ExchangeOnlineManagement
+#Initiates a connection to Exchange Online with PowerShell V2 module
+Connect-ExchangeOnline -ShowProgress $true 
+
+#Put all (-ALL) users with property UserPrincipalName into variable that are:
+#-EnabledFilter EnabledOnly <- only enabled users
+#| WHERE IMMUTABLEID -eq $NULL" <- not in on-prem AD (implicates only cloud users)
+#{$_.UserType -NE "Guest"}  <- not guests
+#{$_.FirstName -NE $NULL } <- filled with name
+
+$Users = GET-MSOLUSER -EnabledFilter EnabledOnly -ALL | WHERE IMMUTABLEID -eq $NULL | ? {$_.UserType -NE "Guest"} | ? {$_.FirstName -NE $NULL } | Select-Object -expandproperty UserPrincipalName 
+#variable to make exclude some users
+$ExcludedUsers = @();
+
+#Exchange room type users
+$ExcludedRoomMailbox =  Get-EXOMailbox -Filter '(RecipientTypeDetails -eq "RoomMailBox")' | Select-Object -expandproperty UserPrincipalName 
+
+
+#for each user in array make password change
+foreach ( $user in $users ) {
+    #if user was excluded then skip
+    if ($ExcludedUsers.Contains($user)){continue}
+    
+    #-ForceChangePassword:$true <- random new password, and promnt to change after first logon
+    #-ForceChangePassword:$true -ForceChangePasswordOnly:$true <- dont change password, and promnt to change after first logon
+
+Set-MsolUserPassword -UserPrincipalName $user -ForceChangePassword:$true -ForceChangePasswordOnly:$true
+
+}
